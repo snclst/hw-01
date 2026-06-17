b9b568ed01dd4c9cbcee409881b3354692af72f6
RamaDev <specopforce@gmail.com>
2026-06-17 19:17:05 +0300

    remove comment

--- app/api/routers/categories.py
+++ app/api/routers/categories.py
@@ -5,21 +5,21 @@ from app.schemas.cats_scem import CategorySchema, CategoryCreateSchema
 from app.services.categories import CatsNotFoundError, CategoryService
 
 
-routers = APIRouter(prefix='/categories', tags=['categories'])
+router = APIRouter(prefix='/categories', tags=['categories'])
 
 
-@routers.get("", response_model=list[CategorySchema])
+@router.get("", response_model=list[CategorySchema])
 def get_categories(service: CategoryService = Depends(get_category_service)) -> list[CategorySchema]:
     return service.list_category()
 
-@routers.post("", response_model=CategorySchema, status_code=status.HTTP_201_CREATED)
+@router.post("", response_model=CategorySchema, status_code=status.HTTP_201_CREATED)
 def create_category(
     payload: CategoryCreateSchema,
     service: CategoryService = Depends(get_category_service),
 ) -> CategorySchema:
     return service.create_category(payload)
 
-@routers.patch("/{category_id}", response_model=CategorySchema)
+@router.patch("/{category_id}", response_model=CategorySchema)
 def update_category(
     category_id: str,
     payload: CategorySchema,
@@ -34,7 +34,7 @@ def update_category(
         )
 
 
-@routers.delete("/{category_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{category_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_category(
     category_id: str,
     service: CategorySchema = Depends(get_category_service),
